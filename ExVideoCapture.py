056dfaa8f6568aaaea77ffddff6e860228605b58
venture21 <park0122@gmail.com>
2024-09-02 14:52:21 +0900

    Add exVideoCapture.py

---
+++ ExVideoCapture.py
@@ -0,0 +1,39 @@
+# 이미지를 불러올때는 imread()
+# 동영상을 불러올때는 VideoCapture()
+
+import cv2, sys
+
+fileName = 'data/vtest.avi'
+
+# 동영상 파일 열기
+cap = cv2.VideoCapture(fileName)
+
+# 동영상의 해상도 width, height 확인
+frameSize = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), \
+    int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(frameSize)
+
+# 동영상 이미지를 다 가져올때까지 반복
+while(True):
+    # 동영상에서 한장의 이미지를 가져오기
+    # retval : 동영상에서 이미지 가져올때 정상 동작 했나?  
+    # frame은 이미지 한장 
+    # 동영상 코덱 디코딩도 포함
+    retval, frame = cap.read()
+    # retval가 양수가 아니면 while문 빠져나가기(종료)
+    if not retval:
+        break
+    cv2.imshow('frame', frame)
+    
+    # 100ms대기 (이 동영상은 초당 10프레임 짜리니까)
+    key = cv2.waitKey(100)
+    # 키 입력이 ESC(27)이면 종료
+    if key==27: # ESC
+        break
+
+# 동영상을 열었으면, 닫아야 한다.
+if cap.isOpened():
+    cap.release() # 열림 해제
+    
+cv2.destroyAllWindows()
+
--- gemini_ex.py
+++ gemini_ex.py
@@ -1,6 +1,3 @@
-# 이미지를 4장 불러온다
-# 이미지를 4장을 하나의 창에 띄운다.
-
 import cv2, sys
 from matplotlib import pyplot as plt
 
@@ -19,20 +16,28 @@ imgRGB2 = cv2.cvtColor(imgBGR2, cv2.COLOR_BGR2RGB)
 imgRGB3 = cv2.cvtColor(imgBGR3, cv2.COLOR_BGR2RGB)
 imgRGB4 = cv2.cvtColor(imgBGR4, cv2.COLOR_BGR2RGB)
 
-#matplotlib plt.subplots로 이미지를 출력
+# matplotlib plt.subplots로 이미지를 출력
 figsize = (7,7)
 fig, ax = plt.subplots(2,2, figsize=figsize)
+
+# 각 이미지에 대한 제목 설정
+ax[0][0].imshow(imgRGB1)
+ax[0][0].set_title("Lena")
 ax[0][0].axis('off')
+
+ax[0][1].imshow(imgRGB3)
+ax[0][1].set_title("Apple")
 ax[0][1].axis('off')
+
+ax[1][0].imshow(imgRGB4)
+ax[1][0].set_title("Baboon")
 ax[1][0].axis('off')
+
+ax[1][1].imshow(imgRGB2)
+ax[1][1].set_title("Orange")
 ax[1][1].axis('off')
 
-ax[0][0].imshow(imgRGB1, aspect='auto')
-ax[0][1].imshow(imgRGB3, aspect='auto')
-ax[1][0].imshow(imgRGB4, aspect='auto')
-ax[1][1].imshow(imgRGB2, aspect='auto')
+# 전체 Figure 제목 설정
+fig.suptitle("Image Gallery")
 
-fig.canvas.manager.set_window_title('Sample windows')
 plt.show()
-
-
