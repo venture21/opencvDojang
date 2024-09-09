0e158790ed6128adad9dabbddcbff6acd79e9ca9
venture21 <park0122@gmail.com>
2024-09-09 15:04:40 +0900

    shear함수 수정

--- exWarpAffine.py
+++ exWarpAffine.py
@@ -16,7 +16,7 @@ def shear(src, x_shear=0,y_shear=0):
     elif y_shear>0 and x_shear==0:
         aff = np.array([[1, 0, 0],[y_shear, 1, 0]], dtype=np.float32)
         h, w = src.shape[:2]
-        dst = cv2.warpAffine(src,aff, w, (h+int(w * y_shear)))    
+        dst = cv2.warpAffine(src,aff, (w, h+int(w * y_shear)))    
     
     return dst
 
@@ -24,7 +24,7 @@ def shear(src, x_shear=0,y_shear=0):
 def scale(src, x_scale,y_scale):
     h, w = src.shape[:2]
     aff = np.array([[x_scale, 0, 0],[0, y_scale, 0]], dtype=np.float32)
-    dst = cv2.warpAffine(src,aff, (w, h))
+    dst = cv2.warpAffine(src,aff, (int(w*x_scale), int(h*y_scale)))
     return dst
 
 src = cv2.imread('data/lenna.bmp')
@@ -34,12 +34,12 @@ if src is None:
 
 print(src.shape)
 #dst = translate(src,50,50)
-#dst = shear(src, 0.5, 0)
+dst = shear(src, 0, 0.5)
 #dst = scale(src, 1.5, 1.5)
 # 512x512 -> 1024x1024 해상도로 설정
 #dst = cv2.resize(src,(1024,1024))
 # 비율로 설정(해상도입력X)
-dst = cv2.resize(src,(0,0), fx=1.5,fy=1.5) 
+#dst = cv2.resize(src,(0,0), fx=1.5,fy=1.5) 
 
 cv2.imshow('src',src)
 cv2.imshow('dst',dst)
